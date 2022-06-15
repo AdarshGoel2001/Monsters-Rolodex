1cf9d3214af1c5e69ef69e8783b14f184f9555b9
Adarsh Goel BitsP <f20200635@pilani.bits-pilani.ac.in>
2022-06-15 08:09:13 +0530

    adding all files to repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { CardList } from "./components/card-list/card-list.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
+import { SearchBox } from "./components/card-list/search box/searchbox.jsx";
+import "./App.css";
+//https://robohash.org/7?set=set2
+//https://jsonplaceholder.typicode.com/users
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const loadNames = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        setMonsters(myJson);
+      });
+  };
+
+  useEffect(() => {
+    loadNames();
+  }, []);
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        placeholder="search monsters"
+        handlechange={(e) => {
+          setSearchField(e.target.value);
+        }}
+      ></SearchBox>
+      {filteredMonsters.length > 0 && (
+        <CardList monsters={filteredMonsters}></CardList>
+      )}
     </div>
   );
 }
